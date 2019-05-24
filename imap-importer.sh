d2a1efd2a62967e789855e43501f1961bc0398d0
Stefan Eiermann <github@ultraapp.de>
2019-05-24 17:00:39 +0200

    Update imap-importer.sh

--- imap-importer.sh
+++ imap-importer.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 
-attachment-downloader --host ${DMARC_VIEWER_IMAP_HOST} --username ${DMARC_VIEWER_IMAP_USER} --password ${DMARC_VIEWER_IMAP_PASS} --imap-folder ${DMARC_VIEWER_IMAP_FOLDER} --output /imapbox
+attachment-downloader --host ${DMARC_VIEWER_IMAP_HOST} --username ${DMARC_VIEWER_IMAP_USER} --delete --password ${DMARC_VIEWER_IMAP_PASS} --imap-folder ${DMARC_VIEWER_IMAP_FOLDER} --output /imapbox
 
 find /imapbox -type f -iname *.zip -exec unzip {} -d /imapbox \;
 
